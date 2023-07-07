b75206162f6430ef7beed8e4d58bca038e886e98
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-07 20:23:25 +0300

    added pokeapi fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
-{#each generations as generation}
+{#each data.monsters as monster (monster.id)}
+  <p>{monster.id}: {monster.name}</p>
+{/each}
+
+{#each generations as generation (generation.id)}
   <h1>{generation.name}</h1>
   <p>Games: {generation.games.join(', ')}</p>
   <p>Main region: {generation.main_region}</p>
 {/each}
 
-<script lang="ts">
-import {generations} from './generations';
+<script lang='ts'>
+  import type { PageData } from './$types';
+  import { generations } from './generations';
+
+  export let data: PageData;
 </script>
