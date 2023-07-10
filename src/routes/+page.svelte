aa5a5856be5ad2205bb34e657abb676564d47d35
Peter Weiser <peterweiser87@gmail.com>
2023-07-10 20:43:58 +0300

    added primary and secondary selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import type { IndexMonster } from './+page';
+	import { page } from '$app/stores';
 	import { generations } from './generations';
+	import { goto } from '$app/navigation';
 
 	export let data: PageData;
+	$: monsterID = $page.url.searchParams.get('monsterID') || '';
+	$: monster = data.monsters.find((monster) => monster.id === monsterID);
+	$: monster2ID = $page.url.searchParams.get('monster2ID') || '';
+	$: monster2 = data.monsters.find((monster) => monster.id === monster2ID);
+
+	const updateSearchParams = (key: string, value: string) => {
+		const searchParams = new URLSearchParams($page.url.searchParams);
+		searchParams.set(key, value);
+		goto(`?${searchParams.toString()}`);
+	};
 </script>
+
+<h1>{monsterID}</h1>
+<h3>{monster?.name}</h3>
+<h1>{monster2ID}</h1>
+<h3>{monster2?.name}</h3>
 
 <div class="generations">
 	{#each generations as generation (generation.id)}
@@ -16,11 +34,20 @@
 <div class="monsters">
 	{#each data.monsters as monster (monster.id)}
 		<div class="monster">
-			<div class="monster-content">
-				<img src={monster.image} alt={monster.name} />
-				<p>{monster.name}</p>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div on:click={() => updateSearchParams('monsterID', monster.id)}>
+				<div class="monster-content">
+					<img src={monster.image} alt={monster.name} />
+					<p>{monster.name}</p>
+				</div>
+				<div class="monster-id">{monster.id}</div>
 			</div>
-			<div class="monster-id">{monster.id}</div>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<button class="secondary" on:click={() => updateSearchParams('monster2ID', monster.id)}>
+				Add as secondary
+			</button>
 		</div>
 	{/each}
 </div>
@@ -60,6 +87,7 @@
 		padding: 10px;
 		position: relative;
 		background-color: #eee;
+		cursor: pointer;
 	}
 
 	.monster:hover {
@@ -79,5 +107,15 @@
 		font-size: 0.8em;
 		font-weight: bold;
 		color: #888;
+	}
+
+	.secondary {
+		text-align: center;
+		background-color: #ddd;
+		cursor: pointer;
+	}
+
+	.secondary:hover {
+		background-color: #eee;
 	}
 </style>
